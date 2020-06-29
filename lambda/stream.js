b5cc17932e35bf202acafdb535a9212ed1df3587
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-06-29 16:35:00 +0200

    Add url and metadata of the stream

---
+++ lambda/stream.js
@@ -0,0 +1,20 @@
+const stream = {
+  "token": "1",
+  "url": "[stream url]",
+  "metadata": {
+    "title": "[title]",
+    "subtitle": "[subtitle]",
+    "art": {
+      "sources": [{
+        "url": "[image url]"
+      }]
+    },
+    "backgroundImage": {
+      "sources": [{
+        "url": "[background image url]",
+      }]
+    }
+  }
+}
+
+module.exports = { stream };
